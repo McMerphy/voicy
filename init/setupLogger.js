08b30c83b6fcb5bb102fc67689bd1ce68ab4977d
alexeym <marozaliaksei@gmail.com>
2021-08-22 15:03:36 +0300

    hotfix: logfile path

--- init/setupLogger.js
+++ init/setupLogger.js
@@ -4,7 +4,7 @@ function setupLogger() {
     log4js.configure({
         appenders: {
             default: { type: 'console' },
-            cheese: { type: 'file', filename: process.env.LOG_PATH | 'voicylog.log' }
+            cheese: { type: 'file', filename: process.env.LOG_PATH || 'voicylog.log' }
         },
         categories: { default: { appenders: ["cheese", "default"], level: "info" } }
     });
