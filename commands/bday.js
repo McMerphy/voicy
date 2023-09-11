7ca069d8234aee2c55e71cfb0f81a6e6a7cd2bd6
alexeym <alexei.moroz@sensotornica.com>
2023-09-11 23:00:41 +0300

    Add a complete list of birthdays

--- commands/bday.js
+++ commands/bday.js
@@ -7,6 +7,11 @@ function setupBday(bot) {
     await handle(ctx)
   })
 
+  // bot.command('addbday', async ctx => {
+  //   const message = ctx.message || ctx.update.channel_post
+  //   let words = message.text.split(' ')
+  //   let username = words[0]
+  // })
 }
 
 async function handle(ctx) {
